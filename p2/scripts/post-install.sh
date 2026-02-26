75fcabf070be356e2813c5145bc502124b133864
Christella <desousa.christella@proton.me>
2026-02-26 13:19:51 +0100

    feat(p2): add files to web apps and deploy with volumes

--- p2/scripts/post-install.sh
+++ p2/scripts/post-install.sh
@@ -2,6 +2,9 @@
 
 SERVER_NAME=$1
 
-vagrant ssh $SERVER_NAME -c "sudo kubectl apply -f /confs/services.yml \
+vagrant ssh $SERVER_NAME -c "sudo kubectl create configmap app-one-files --from-file=/websites/app1 \
+&& sudo kubectl create configmap app-two-files --from-file=/websites/app2 \
+&& sudo kubectl create configmap app-three-files --from-file=/websites/app3 \
+&& sudo kubectl apply -f /confs/services.yml \
 && sudo kubectl apply -f /confs/deployments.yml \
 && sudo kubectl apply -f /confs/ingresses.yml"
